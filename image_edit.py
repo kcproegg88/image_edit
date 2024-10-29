6e088b775ecc5f82b2d0e1137c3e922f41e5977c
krishna <krischauhan01@gmail.com>
2024-10-29 17:24:14 -0400

    image quality reducer works

--- image_edit.py
+++ image_edit.py
@@ -1,18 +1,36 @@
 from PIL import Image
-
+import numpy as np
+import math
 
 def resize_width(img: Image, new_width: int):
     w, l = img.size
     return img.resize((new_width, int(l*new_width/w)))
 
 
-def map_pixels(img: Image, image_func=lambda x:x, line=False):
+def map_pixels(img: Image, image_func=lambda x, **kwargs: x, line=False, **kwargs):
     w, l = img.size
     pixels = img.load()
     if not line:
-        return [[image_func(pixels[x, y]) for y in range(l)] for x in range(w)]
+        return [[image_func(pixels[x, y], **kwargs) for y in range(l)] for x in range(w)]
     else:
-        return [image_func([pixels[x, y] for y in range(l)]) for x in range(w)]
+        return [image_func([pixels[x, y] for y in range(l)], **kwargs) for x in range(w)]
+
+
+def low_def(c, division=4):
+    d = 256/division
+    r, g, b = math.ceil(c[0]/d)*d, math.ceil(c[1]/d)*d, math.ceil(c[2]/d)*d
+    if r == 0: r = 1
+    if g == 0: g = 1
+    if b == 0: b = 1
+    return r-1, g-1, b-1
+
+
+def transpose(array):
+    new_array = [[] for i in range(len(array[0]))]
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            new_array[j].append(array[i][j])
+    return new_array
 
 
 def separate_rgb(pixels_array):
@@ -29,6 +47,26 @@ def separate_rgb(pixels_array):
         b_array.append(b_line)
     return r_array, g_array, b_array
 
+
+def separate_rgb_zeros(pixels_array):
+    r_array, g_array, b_array = [], [], []
+    for i in range(len(pixels_array)):
+        r_line, g_line, b_line = [], [], []
+        for j in range(len(pixels_array[0])):
+            r, g, b = pixels_array[i][j]
+            r_line.append((r, 0,0))
+            g_line.append((0, g, 0))
+            b_line.append((0, 0, b))
+        r_array.append(r_line)
+        g_array.append(g_line)
+        b_array.append(b_line)
+    return r_array, g_array, b_array
+
+
+def single_colour_image(img, color: int):
+    for i in range(3):
+        img = Image.fromarray(np.array(transpose(separate_rgb_zeros(map_pixels(img))[i]), dtype=np.uint8), mode='RGB')
+        img.show()
 
 def write_2d(file_name: str, lines):
     with open(file_name, "a") as file:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 from image_edit import *
 img = Image.open("mona_lisa.jpg")
 
@@ -23,10 +24,22 @@ print("Image size:", img_resized.size)  # (width, height)
 print("Image format:", img_resized.format)  # e.g., JPEG, PNG
 print("Image mode:", img_resized.mode)  # e.g., RGB, L (grayscale)
 
-rgb_sep = separate_rgb(map_pixels(img_resized))
+rgb_sep = separate_rgb(transpose(map_pixels(img_resized)))
 rgb_text_list = "r_text.txt", "g_text.txt", "b_text.txt"
-for i in range(3):
-    write_2d(rgb_text_list[i], rgb_sep[i])
+# for i in range(3):
+#     write_2d(rgb_text_list[i], rgb_sep[i])
 
-print(len(("80 98 106 108 114 120 124 128 137 141 143 149 151 154 148 114 120 125 124 120 94 91 96 92 89 84 83 88 105 103 103 100 97 104 102 129 147 134 134 99 92 86 93 92 78 99 102 74 85 87 88 94 91 80 63 61 64 60 56 63 64 76 87 84 74 59 55 53 47 39 37 35 28 26").split()))
+for i in range(1):
+    new_img = Image.fromarray(np.array(transpose(separate_rgb_zeros(map_pixels(img))[i]), dtype=np.uint8), mode='RGB')
+    new_img.show()
+
+for i in range(1):
+    new_img = Image.fromarray(np.array(transpose(separate_rgb_zeros(map_pixels(img, low_def))[i]), dtype=np.uint8), mode='RGB')
+    new_img.show()
+
 # clear_all(rgb_text_list)
+#
+# arr = [['a', 'b'],['c', 'd']]
+# print(*arr, sep="\n")
+#
+# print(*transpose(arr), sep="\n")
